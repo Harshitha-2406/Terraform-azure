868539f49e7aea5aee21db1c9f3adf7a71a0e811
Harshitha S <harshitha.s@koantek.com>
2024-08-28 13:23:21 +0530

    changed the branch in yml

--- UnityCatalog/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "mtech-databricks-terraform-dev"
-    storage_account_name = "mtechterraformstatefile"
-    container_name       = "terraform-statefiles"
-    key                  = "mtechterraformuc.tfstate"
-  }
-}
--- dev-environment/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "Authentica-rg"
-    storage_account_name = "authenticastatefile"
-    container_name       = "terraform-statefiles"
-    key                  = "authenticaterraformdev.tfstate"
-  }
-}
