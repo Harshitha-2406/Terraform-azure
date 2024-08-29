fac70b025a1ee81931548617938108927d3d00a3
Harshitha S <harshitha.s@koantek.com>
2024-08-29 19:49:24 +0530

    changes stg acc

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "Authentica-rg"
-    storage_account_name = "authenticastatefile"
-    container_name       = "terraform-statefiles"
-    key                  = "authenticaterraformdev.tfstate"
+    storage_account_name = "authenticastgacc"
+    container_name       = "authenticacontainer"
+    key                  = "authenticadev.tfstate"
   }
 }
