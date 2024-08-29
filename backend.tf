80c2eb728365e46700b6a53a51e2c0dcedfd8896
Harshitha S <harshitha.s@koantek.com>
2024-08-29 20:07:51 +0530

    push

--- backend.tf
+++ backend.tf
@@ -3,6 +3,7 @@ terraform {
     resource_group_name  = "Authentica-rg"
     storage_account_name = "authenticastgacc"
     container_name       = "authenticacontainer"
+    use_azuread_auth     = true
     key                  = "authenticadev.tfstate"
   }
 }
