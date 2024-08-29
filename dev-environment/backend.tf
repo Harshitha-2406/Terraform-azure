c3ee52018f4f81614ef07d2f5ce8a6720613b49c
Harshitha S <harshitha.s@koantek.com>
2024-08-29 18:10:35 +0530

    added new branch

--- dev-environment/backend.tf
+++ dev-environment/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "Authentica-rg"
-    AZURE_STORAGE_ACCOUNT_NAME = "authenticastatefile"
-    AZURE_CONTAINER_NAME       = "terraform-statefiles"
+    storage_account_name = "authenticastatefile"
+    container_name       = "terraform-statefiles"
     key                  = "authenticaterraformdev.tfstate"
   }
 }
